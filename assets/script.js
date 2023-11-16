ace64979ae5a11d2c3a3a5ca0fae70cbba87c840
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2023-11-16 02:25:56 -0600

    added working javascript code for password gernerator

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,55 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  function generatePassword() {
-    var characterLength = prompt("Enter number of characters", 8);
-    if (characterLength <= 8) {
+  
+  var passwordLength = prompt("Enter number of characters", 8);
+    //makes sure that if password length is too little or too big it does not work for greater password security  
+    if (passwordLength <= 7) {
       alert("Your password must be more than 8 characters");
-      return characterLength
+      return
     }
-    if (characterLength >=128){
+    if (passwordLength >=127){
       alert("Your password must be less than 128 characters");
-    return characterLength
+      return
     }
-    var addUppercase = confirm("Do you wish to add lowercase letters to your password")
+    
+    var addLowercase = confirm("Do you wish to add lowercase letters to your password")
     var addUppercase = confirm("Do you wish to add uppercase letters to your password")
     var addNumbers = confirm("Do you wish to add numbers to your password")
     var addSymbols = confirm("Do you wish to add symbols to your password")
+
+    var lowercaseCharCodes =arrayLowToHigh(97,122)
+    var uppercaseCharCodes =arrayLowToHigh(65,90)
+    var numberCharCodes =arrayLowToHigh(48,57)
+    var symbolCharCodes =arrayLowToHigh(33,47)
+    .concat(arrayLowToHigh(58,64)
+    ).concat(arrayLowToHigh(91,96)
+    ).concat(arrayLowToHigh(124,126)
+    )
+
+  function generatePassword(passwordLength,addUppercase, addNumbers,addSymbols ) {
+    let charCodes =lowercaseCharCodes
+    if(addUppercase) charCodes =lowercaseCharCodes.concat(uppercaseCharCodes)
+    if(addNumbers) charCodes =lowercaseCharCodes.concat(numberCharCodes)
+    if(addSymbols) charCodes =lowercaseCharCodes.concat(symbolCharCodes)
+
+    const passwordCharacters = []
+    for(let i =0; i<passwordLength; i++){
+      const characterCode =charCodes[Math.floor(math.random()* charCodes.length)]
+      passwordCharacters.push(String.fromCharCode(characterCode))
+    }  
+    return passwordCharacters.join('')
+
+  }
+  function arrayLowToHigh(low,high){
+    const array=[]
+    for(let i = low; i<=high; i++){
+      array.push(i)
+    }
+    return array
   }
   passwordText.value = password;
+  console.log(generatePassword)
 }
 
 // Add event listener to generate button
